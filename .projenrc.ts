173dcf98d6bababf4a37c3e1e70e51acf1c0de1d
yicr <yicr@users.noreply.github.com>
2025-01-17 19:00:23 +0900

    chore(deps): add lambda packages

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,8 +10,36 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: '@gammarers/aws-sns-slack-message-lambda-subscription',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-sns-slack-message-lambda-subscription.git',
+  deps: [
+    '@gammarers/aws-resource-naming@0.8.0',
+  ],
+  devDeps: [
+    '@gammarers/jest-aws-cdk-asset-filename-renamer@^0.5.21',
+    '@gammarers/aws-resource-naming@0.8.0',
+    '@slack/webhook@^7.0.3',
+    '@types/aws-lambda@^8.10.145',
+    'axios@^1.6.7',
+    'aws-sdk-client-mock@^4.1.0',
+    'aws-sdk-client-mock-jest@^4.1.0',
+  ],
+  peerDeps: [
+    '@gammarers/aws-resource-naming@^0.8.0',
+  ],
   releaseToNpm: false, // todo: temp
   npmAccess: javascript.NpmAccess.PUBLIC,
+  jestOptions: {
+    jestConfig: {
+      snapshotSerializers: ['<rootDir>/node_modules/@gammarers/jest-aws-cdk-asset-filename-renamer'],
+    },
+    extraCliOptions: ['--silent'],
+  },
+  lambdaOptions: {
+    runtime: awscdk.LambdaRuntime.NODEJS_20_X,
+    bundlingOptions: {
+      externals: ['@aws-sdk/*'],
+      sourcemap: true,
+    },
+  },
   minNodeVersion: '18.0.0',
   workflowNodeVersion: '22.4.x',
   depsUpgradeOptions: {
